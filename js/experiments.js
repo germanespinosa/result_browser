66cc8c301f0e67e47118300f226390f249bdd18d
germanespinosa <germanespinosa@gmail.com>
2020-06-13 07:20:11 -0500

    none

--- js/experiments.js
+++ js/experiments.js
@@ -1,17 +1,20 @@
 window.onload = function () {
-    let experiment_name = parameters.experiment;
-    let group_name = parameters.group;
-    let wold_name = parameters.world;
-    let set_name = parameters.set;
     let title = document.getElementById("title");
-    title.innerHTML = parameters.world;
-    loadSettings();
+    title.innerHTML = "experiments";
     load_experiments();
 };
 
 
 function load_experiments(){
     loadfile(results_folder + '/experiments.json', function(experiments) {
-
+        let content = document.getElementById("content");
+        let HTML = '<div>';
+        for (let i=0; i<experiments.length; i++) {
+            HTML += "<div>";
+            HTML += "<div><a href='experiment.html?experiment=" + experiments[i].experiment_name + "'>" + experiments[i].experiment_name + "</a></div>";
+            HTML += "</div>";
+        }
+        HTML += "</div>";
+        content.innerHTML = HTML;
     });
 }
